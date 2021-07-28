7c43dc2a0454b58b950089a673d2ab26ee3b15c4
dizzyi <deeralan827@gmail.com>
2021-07-28 17:04:13 +0800

    added 1 test photo compiled jupyter notebook file to py file

--- binPicking/script/detectron/Test.py
+++ binPicking/script/detectron/Test.py
@@ -12,7 +12,6 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.data.datasets import register_coco_instances
 
 # In[ ]:
 
